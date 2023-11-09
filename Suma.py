4626a6902ab018d576ed41cbe4e0e47adc5d029d
draberlong <33308047+draberlong@users.noreply.github.com>
2023-11-09 16:24:54 -0600

    Add files via upload

---
+++ Suma.py
@@ -0,0 +1,18 @@
+# Este es un ejemplo de código de Python
+
+def suma(a, b):
+    """
+    Suma dos números
+
+    Args:
+        a: Primer número
+        b: Segundo número
+
+    Returns:
+        La suma de los dos números
+    """
+
+    return a + b
+
+
+print(suma(1, 2))
